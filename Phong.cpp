667ec0e4562b2dab976d68d75b768ace8671edb8
U-Alisha-PC\Alisha <Alisha@Alisha-PC.(none)>
2011-01-22 12:17:27 -0800

    Added solution and Phong files(not implemented yet)

---
+++ Phong.cpp
@@ -0,0 +1,45 @@
+#include "Phong.h"
+#include "Ray.h"
+#include "Scene.h"
+
+Phong::Phong(const Vector3 &kd, const Vector3 &ka, const Vector3 &ks, const float a)
+	:m_kd(kd), m_ka(ka), m_ks(ks), a(0.f)
+{
+
+}
+
+Phong::~Phong()
+{
+
+}
+
+Vector3 
+Phong::shade(const Ray &ray, const HitInfo &hit, const Scene &scene) const
+{
+	Vector3 L = Vector3(0.0f, 0.0f, 0.0f);
+
+	Vector3 viewDir = -ray.d;
+
+	const Lights *lightlist = scene.lights();
+
+	// loop over all of the lights
+    Lights::const_iterator lightIter;
+    for (lightIter = lightlist->begin(); lightIter != lightlist->end(); lightIter++)
+    {
+        PointLight* pLight = *lightIter;
+    
+        Vector3 surfaceToLight = pLight->position() - hit.P;
+        
+        // the inverse-squared falloff
+        float falloff = l.length2();
+        
+        // normalize the light direction
+        l /= sqrt(falloff);
+
+		Vector3 lightToSurface = -surfaceToLight;
+
+		Vector3 reflectRay = lightToSurface - 2 * cross(lightToSurface, hit.N) * hit.N;
+
+
+}
+
---
+++ Phong.h
@@ -0,0 +1,37 @@
+#ifndef CSE168_PHONG_H_INCLUDED
+#define CSE168_PHONG_H_INCLUDED
+
+#include "Material.h"
+
+class Phong : public Material
+{
+public:
+	Phong(const Vector3 & kd = Vector3(1), 
+			const Vector3 & ka = Vector3(0),
+			const Vector3 & ks = Vector(1),
+			const float a = 0.f);
+	virtual ~Phong();
+
+	const Vector3 & kd() const {return m_kd;}
+	const Vector3 & ka() const {return m_ka;}
+	const Vector3 & ks() const {return m_ks;}
+	const float a() const {return a;}
+
+	void setKd(const Vector3 & kd) {m_kd = kd;}
+	void setKa(const Vector3 & ka) {m_ka = ka;}
+	void setKs(const Vector3 & ks) {m_ks = ks;}
+	void setA(const float a) {m_a = a;}
+
+    virtual void preCalc() {}
+    
+    virtual Vector3 shade(const Ray& ray, const HitInfo& hit,
+                          const Scene& scene) const;
+
+private:
+	Vector3 m_kd;
+	Vector3 m_ka;
+	Vector3 m_ks;
+	float m_a;
+};
+
+#endif		// CSE168_PHONG_H_INCLUDED
