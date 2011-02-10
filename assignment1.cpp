aca4372eb2d115ec7d8f5667196c6861e3f0db2c
Hallgeir Lien <cse260-04@cseapel05.ucsd.edu>
2011-02-10 09:33:47 -0800

    Added assignment1 files from website.w

---
+++ assignment1.cpp
@@ -0,0 +1,179 @@
+#include "assignment1.h"
+#include <math.h>
+#include "Miro.h"
+#include "Scene.h"
+#include "Camera.h"
+#include "Image.h"
+#include "Console.h"
+
+#include "PointLight.h"
+#include "TriangleMesh.h"
+#include "Triangle.h"
+#include "Lambert.h"
+
+void
+makeBunnyScene()
+{
+    g_camera = new Camera;
+    g_scene = new Scene;
+    g_image = new Image;
+
+    g_image->resize(128, 128);
+    
+    // set up the camera
+    g_camera->setBGColor(Vector3(0.0f, 0.0f, 0.2f));
+    g_camera->setEye(Vector3(-2, 3, 5));
+    g_camera->setLookAt(Vector3(-.5, 1, 0));
+    g_camera->setUp(Vector3(0, 1, 0));
+    g_camera->setFOV(45);
+
+    // create and place a point light source
+    PointLight * light = new PointLight;
+    light->setPosition(Vector3(-3, 15, 3));
+    light->setColor(Vector3(1, 1, 1));
+    light->setWattage(500);
+    g_scene->addLight(light);
+
+    Material* mat = new Lambert(Vector3(1.0f));
+
+    TriangleMesh * bunny = new TriangleMesh;
+    bunny->load("bunny.obj");
+    
+    // create all the triangles in the bunny mesh and add to the scene
+    for (int i = 0; i < bunny->numTris(); ++i)
+    {
+        Triangle* t = new Triangle;
+        t->setIndex(i);
+        t->setMesh(bunny);
+        t->setMaterial(mat); 
+        g_scene->addObject(t);
+    }
+    
+    // create the floor triangle
+    TriangleMesh * floor = new TriangleMesh;
+    floor->createSingleTriangle();
+    floor->setV1(Vector3(  0, 0,  10));
+    floor->setV2(Vector3( 10, 0, -10));
+    floor->setV3(Vector3(-10, 0, -10));
+    floor->setN1(Vector3(0, 1, 0));
+    floor->setN2(Vector3(0, 1, 0));
+    floor->setN3(Vector3(0, 1, 0));
+    
+    Triangle* t = new Triangle;
+    t->setIndex(0);
+    t->setMesh(floor);
+    t->setMaterial(mat); 
+    g_scene->addObject(t);
+    
+    // let objects do pre-calculations if needed
+    g_scene->preCalc();
+}
+
+void
+makeSphereScene()
+{
+    g_camera = new Camera;
+    g_scene = new Scene;
+    g_image = new Image;
+
+    g_image->resize(128, 128);
+    
+    // set up the camera
+    g_camera->setBGColor(Vector3(0.0f, 0.0f, 0.2f));
+    g_camera->setEye(Vector3(-2, 3, 5));
+    g_camera->setLookAt(Vector3(-.5, 1, 0));
+    g_camera->setUp(Vector3(0, 1, 0));
+    g_camera->setFOV(45);
+
+    // create and place a point light source
+    PointLight * light = new PointLight;
+    light->setPosition(Vector3(-3, 15, 3));
+    light->setColor(Vector3(1, 1, 1));
+    light->setWattage(500);
+    g_scene->addLight(light);
+
+    Material* mat = new Lambert(Vector3(1.0f));
+
+    Sphere *sphere = new Sphere;
+    sphere->setRadius(1.5);
+
+    // create the floor triangle
+    TriangleMesh * floor = new TriangleMesh;
+    floor->createSingleTriangle();
+    floor->setV1(Vector3(  0, 0,  10));
+    floor->setV2(Vector3( 10, 0, -10));
+    floor->setV3(Vector3(-10, 0, -10));
+    floor->setN1(Vector3(0, 1, 0));
+    floor->setN2(Vector3(0, 1, 0));
+    floor->setN3(Vector3(0, 1, 0));
+    
+    Triangle* t = new Triangle;
+    t->setIndex(0);
+    t->setMesh(floor);
+    t->setMaterial(mat); 
+    g_scene->addObject(t);
+    g_scene->addObject(sphere);
+    
+    // let objects do pre-calculations if needed
+    g_scene->preCalc();
+}
+
+void
+makeTeapotScene()
+{
+    g_camera = new Camera;
+    g_scene = new Scene;
+    g_image = new Image;
+
+    g_image->resize(128, 128);
+    
+    // set up the camera
+    g_camera->setBGColor(Vector3(0.0f, 0.0f, 0.2f));
+    g_camera->setEye(Vector3(-2, 3, 5));
+    g_camera->setLookAt(Vector3(-.5, 1, 0));
+    g_camera->setUp(Vector3(0, 1, 0));
+    g_camera->setFOV(45);
+
+    // create and place a point light source
+    PointLight * light = new PointLight;
+    light->setPosition(Vector3(-3, 15, 3));
+    light->setColor(Vector3(1, 1, 1));
+    light->setWattage(500);
+    g_scene->addLight(light);
+
+    Material* mat = new Lambert(Vector3(1.0f));
+
+    TriangleMesh * teapot = new TriangleMesh;
+    teapot->load("teapot.obj");
+    
+    // create all the triangles in the bunny mesh and add to the scene
+    for (int i = 0; i < bunny->numTris(); ++i)
+    {
+        Triangle* t = new Triangle;
+        t->setIndex(i);
+        t->setMesh(bunny);
+        t->setMaterial(mat); 
+        g_scene->addObject(t);
+    }
+    
+    // create the floor triangle
+    TriangleMesh * floor = new TriangleMesh;
+    floor->createSingleTriangle();
+    floor->setV1(Vector3(  0, 0,  10));
+    floor->setV2(Vector3( 10, 0, -10));
+    floor->setV3(Vector3(-10, 0, -10));
+    floor->setN1(Vector3(0, 1, 0));
+    floor->setN2(Vector3(0, 1, 0));
+    floor->setN3(Vector3(0, 1, 0));
+    
+    Triangle* t = new Triangle;
+    t->setIndex(0);
+    t->setMesh(floor);
+    t->setMaterial(mat); 
+    g_scene->addObject(t);
+    
+    // let objects do pre-calculations if needed
+    g_scene->preCalc();
+}
+
+
