f4f09a3cde02e02324f673738617ca3ac10729d6
Alisha <alawson@cs.ucsd.edu>
2011-02-24 10:07:15 -0800

    added stats

---
+++ Stats.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Stats.h"
+
+int Stats::BVH_Nodes = 0;
+int Stats::BVH_LeafNodes = 0;
+int Stats::Rays = 0;
+int Stats::Primary_Rays = 0;
+int Stats::Secondary_Rays = 0;
+int Stats::Ray_Box_Intersect = 0;
+int Stats::Ray_Tri_Intersect = 0;
+
+void Stats::PrintStats()
+{
+	printf("\n~~Ray Tracer Stats~~\n\n");
+	printf("BVH Nodes: %d\n", BVH_Nodes);
+	printf("BVH Leaf Nodes: %d\n", BVH_LeafNodes);
+	printf("Rays: %d\n", Rays);
+	printf("Ray-Box Intersections: %d\n", Ray_Box_Intersect);
+	printf("Ray-Triangle Intersections: %d\n", Ray_Tri_Intersect);
+}
