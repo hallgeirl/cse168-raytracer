c1b5d08af676f0eadaee07c5e79a42cb452d276e
Hallgeir Lien <hallgeir.lien@gmail.com>
2011-02-24 14:55:22 -0800

    fixed shadows in lambert

--- Lambert.cpp
+++ Lambert.cpp
@@ -34,13 +34,14 @@ Lambert::shade(const Ray& ray, const HitInfo& hit, const Scene& scene) const
         // normalize the light direction
         l /= sqrt(falloff);
 
+#ifndef DISABLE_SHADOWS
 		Ray Shadow(hit.P+(l*epsilon), l);
 		HitInfo hitInfo;
 		if (scene.trace(hitInfo, Shadow, 0.f, sqrt(falloff)))
 		{
 			continue;
 		}
-
+#endif
         // get the diffuse component
         float nDotL = dot(hit.N, l);
         Vector3 result = pLight->color();
